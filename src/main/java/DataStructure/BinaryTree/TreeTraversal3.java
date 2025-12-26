b488154afbb4a6c903cf3b72430c3f7bff0f4e4b
Steve95271 <124816688+Steve95271@users.noreply.github.com>
2025-12-26 14:28:40 +0800

    Update TreeTraversal3.java

--- src/main/java/DataStructure/BinaryTree/TreeTraversal3.java
+++ src/main/java/DataStructure/BinaryTree/TreeTraversal3.java
@@ -5,44 +5,108 @@
  */
 package DataStructure.BinaryTree;
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class TreeTraversal3 {
-    public static void main(String[] args) {
-        /*     1
-         *    / \
-         *   2   3
-         *  /   / \
-         * 4   5   6   */
+    /*     1
+     *    / \
+     *   2   3
+     *  /   / \
+     * 4   5   6
+     *
+     * In order: 4 2 7 1 5 3 6
+     * */
+    private static final TreeNode<Integer> root = new TreeNode(
+            new TreeNode(
+                    new TreeNode(4),
+                    2,
+                    new TreeNode(7)),
+            1,
+            new TreeNode(
+                    new TreeNode(5),
+                    3,
+                    new TreeNode(6))
+    );
 
-        TreeNode root = new TreeNode(
-                new TreeNode(new TreeNode(4), 2, new TreeNode(7)),
-                1,
-                new TreeNode(new TreeNode(5), 3, new TreeNode(6))
-        );
+    private static void preOrderTraversal(TreeNode<Integer> root) {
+        TreeNode<Integer> current = root;
+        Deque<TreeNode<Integer>> stack = new LinkedList<>();
 
-        //Current node
-        TreeNode current = root;
+        while (current != null) {
+            System.out.println(current.val);
+            stack.push(current);
+            current = current.left;
 
-        //last node be pop
-        TreeNode pop = null;
-
-        //By using stack for remember the way back
-        LinkedList <TreeNode> stack = new LinkedList<>();
-
-        while(current != null || !stack.isEmpty()){
-            if (current != null) {
-                stack.push(current); //push to stack to remember the way back
-                current = current.left;
-            }else {
-                TreeNode peek = stack.peek();
-                if (peek.right == null || peek.right == pop) {
-                    pop = stack.pop();
-                    System.out.println("back: " + pop.val);
-                }else {
-                    current = peek.right;
+            while (current == null && !stack.isEmpty()) {
+                TreeNode<Integer> pop = stack.pop();
+                if (pop.right != null) {
+                    current = pop.right;
                 }
             }
         }
     }
+
+    private static void inOrderTraversal(TreeNode<Integer> root) {
+        TreeNode<Integer> current = root;
+        Deque<TreeNode<Integer>> stack = new LinkedList<>();
+
+        while (current != null) {
+            stack.push(current);
+            current = current.left;
+
+            while (current == null && !stack.isEmpty()) {
+                TreeNode<Integer> pop = stack.pop();
+                System.out.println(pop.val);
+                if (pop.right != null) {
+                    current = pop.right;
+                }
+            }
+        }
+    }
+
+    private static void postOrderTraversal(TreeNode<Integer> root) {
+        /*      1
+         *     / \
+         *    2   3
+         *  / \   / \
+         * 4  7  5   6
+         *
+         * 后序遍历：
+         *  - 顺序左右值
+         *  - 遍历结果应该为: 4 7 2 5 6 3 1
+         *
+         * stack: Top <- [2 1] -> Bottom
+         * peek = 2
+         * pop = 7
+         * result: 4
+         **/
+        TreeNode<Integer> current = root;
+        Deque<TreeNode<Integer>> stack = new LinkedList<>();
+        // 记录最近一次弹栈元素
+        TreeNode<Integer> pop = null;
+        while (current != null) {
+            stack.push(current);
+            current = current.left;
+
+            while (current == null && !stack.isEmpty()) {
+                TreeNode<Integer> peek = stack.peek();
+                if (peek.right == null || peek.right == pop) {
+                    pop = stack.pop();
+                    System.out.println(pop.val);
+                } else {
+                    // 当堆顶元素还有右子节点，更新当前节点并打断内层循环让它进入外层循环的往左遍历
+                    current = peek.right;
+                    // 如果不打断内层循环，则无法遍历到左边的子节点
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        //preOrderTraversal(root);
+        postOrderTraversal(root);
+    }
+
 }
